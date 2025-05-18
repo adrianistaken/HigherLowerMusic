c5d283ed7732df3ab9ada3fc50a4d9413db28663
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-17 21:35:59 -0500

    Create GameOver component

---
+++ frontend/src/components/GameOver.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useGameManagerStore } from '@/stores/gameManagerStore'
+const gameManagerStore = useGameManagerStore();
+</script>
+
+<template>
+    <div>
+        <!-- Hero section -->
+        <div class="hero bg-base-200/0 min-h-screen">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-5xl font-bold">Game Over</h1>
+                    <p class="py-6">You scored {{ gameManagerStore.currentScore }} points!</p>
+                    <button class="btn btn-primary mt-4" @click="gameManagerStore.startNewGame()">Play Again</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
