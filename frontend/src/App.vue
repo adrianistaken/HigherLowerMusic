5e37b3e90d110471854a2b5592f1f4e8d6e41985
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-17 21:36:22 -0500

    Move data from components to store; Large refactor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,218 +1,42 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { reactive, ref, onMounted, computed } from 'vue';
-import ArtistCard from './components/ArtistCard.vue';
-import Header from './components/Header.vue';
+import { onMounted } from 'vue';
 import MainMenu from './components/MainMenu.vue';
-import axios from 'axios';
+import MainGame from './components/MainGame.vue';
+import GameOver from './components/GameOver.vue';
 
-const data = reactive({
-  artistsList: [],
-  storedArtist: {},
-  gameActive: false,
-  showGameOverMessage: false,
-  score: 0,
-  answerCorrect: false,
-  answerIncorrect: false,
+import { useGameManagerStore } from '@/stores/gameManagerStore'
 
-  volumePercent: 40,
-  isMuted: false,
-})
-
-const volume = computed(() => data.isMuted ? 0 : data.volumePercent / 100)
-
-const showListeners = computed(() => {
-  if (data.showGameOverMessage || data.answerCorrect) return true;
-  else return false;
-});
+const gameManagerStore = useGameManagerStore();
 
 onMounted(async () => {
-  await axios({
-    url: `${import.meta.env.VITE_API_URL}/getArtists`,
-    method: 'GET',
-  })
-    .then((response) => {
-      data.artistsList = response.data;
-    })
+  await gameManagerStore.getFirstTwoArtists();
+  gameManagerStore.setActiveArtists(gameManagerStore.firstArtist, gameManagerStore.secondArtist);
 })
-
-const getArtist = async () => {
-  if (data.artistsList.length < 5) {
-    await axios({
-      url: `${import.meta.env.VITE_API_URL}/getArtist`,
-      method: 'GET',
-    })
-      .then(function (response) {
-        data.artistsList.push(response.data);
-        if (data.artistsList.length < 5) getArtist();
-      });
-  }
-}
-
-const startGame = async () => {
-  data.gameActive = true;
-  getArtist();
-}
-
-const newGame = async () => {
-  data.gameActive = true;
-  data.showGameOverMessage = false;
-  data.answerCorrect = false;
-  data.answerIncorrect = false;
-  data.score = 0;
-  data.artistsList.shift();
-  data.artistsList.shift();
-  getArtist();
-}
-
-const answerHigher = () => {
-  var artistOneListeners = parseFloat(data.artistsList[0].listeners.replace(/,/g, ''));
-  var artistTwoListeners = parseFloat(data.artistsList[1].listeners.replace(/,/g, ''));
-
-  if (artistTwoListeners > artistOneListeners || artistOneListeners === artistTwoListeners) {
-    data.score++;
-    data.answerCorrect = true;
-    playSound('/sounds/level-up-short.mp3');
-
-    setTimeout(() => {
-      data.artistsList.shift();
-      data.answerCorrect = false;
-    }, 2000)
-
-    if (data.artistsList.length < 5) getArtist();
-  } else {
-    data.answerIncorrect = true;
-    playSound('/sounds/meep-merp.mp3');
-    setTimeout(() => {
-      data.showGameOverMessage = true;
-    }, 2000)
-  }
-}
-
-const answerLower = () => {
-  var artistOneListeners = parseFloat(data.artistsList[0].listeners.replace(/,/g, ''));
-  var artistTwoListeners = parseFloat(data.artistsList[1].listeners.replace(/,/g, ''));
-
-  if (artistTwoListeners < artistOneListeners || artistOneListeners === artistTwoListeners) {
-    data.score++;
-    data.answerCorrect = true;
-    playSound('/sounds/level-up-short.mp3');
-
-    setTimeout(() => {
-      data.artistsList.shift();
-      data.answerCorrect = false;
-    }, 2000)
-
-    if (data.artistsList.length < 5) getArtist();
-  } else {
-    data.answerIncorrect = true;
-    playSound('/sounds/meep-merp.mp3');
-    setTimeout(() => {
-      data.showGameOverMessage = true;
-    }, 1800)
-
-  }
-}
-
-function playSound(path) {
-  const audio = new Audio(path)
-  audio.volume = data.isMuted ? 0 : data.volumePercent / 100
-  audio.play()
-  console.log('Playing at volume:', audio.volume)
-}
-
-defineExpose({ showListeners });
 </script>
 
 <template>
-  <div>
-    <!-- Intro component -->
-    <div v-if="!data.gameActive"
-      class="w-full h-screen animate-subtle-gradient bg-gradient-to-br from-orange-200 via-green-700 to-blue-900 bg-[length:300%_300%]">
-      <MainMenu @startGame="startGame" />
+  <div
+    class="w-full h-screen animate-subtle-gradient flex justify-center items-center bg-gradient-to-br from-orange-200 via-green-700 to-blue-900 bg-[length:300%_300%]">
+    <!-- Main Menu -->
+    <div v-if="gameManagerStore.currentScreen === 'mainMenu'">
+      <MainMenu />
     </div>
 
-    <Header v-if="data.gameActive" :score="data.score" :volumePercent="data.volumePercent" :isMuted="data.isMuted"
+    <!-- Header -->
+    <!-- <Header v-if="gameManagerStore.currentScreen === 'mainGame'" :score="data.score" :volumePercent="data.volumePercent" :isMuted="data.isMuted"
       @toggleMute="data.isMuted = !data.isMuted" @update-volume="data.volumePercent = $event"
-      @update-muted="data.isMuted = $event" />
+      @update-muted="data.isMuted = $event" /> -->
 
-    <!-- Main game - artists info section -->
-    <div v-if="data.gameActive"
-      class="w-full h-screen animate-subtle-gradient bg-gradient-to-br from-orange-200 via-green-700 to-blue-900 bg-[length:300%_300%]">
+    <!-- Main Game -->
+    <MainGame v-if="gameManagerStore.currentScreen === 'activeGame'">
+    </MainGame>
 
-      <div class="hero bg-base-200/0 min-h-screen">
-        <div class="hero-content text-center">
-          <div class="max-w-5xl">
-            <div class="flex w-full">
-              <div class="card bg-[rgba(32,32,32,0.35)] rounded-box grid grow place-items-center p-6 max-w-[368px]">
-                <ArtistCard v-if="data.gameActive" :artist="data.artistsList[0]" :showListeners="true" />
-              </div>
-              <div class="divider divider-horizontal mx-10">
-
-                <div class="mx-auto my-4 h-10 w-full flex items-center justify-center">
-                  <div class="h-[.5px] w-full"></div>
-                  <div
-                    class="bg-[rgba(32,32,32,0.35)] rounded-full z-10 w-10 h-10 absolute border-neutral-500 border-[1px]">
-                  </div>
-
-                  <!-- Green correct icon -->
-                  <svg v-if="data.answerCorrect" class="result_shape checkmark m-auto absolute z-20"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                    <circle class="result_circle checkmark_circle" cx="26" cy="26" r="25" fill="none" />
-                    <path class="result_outline" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                  </svg>
-                  <!-- Red incorrect icon -->
-                  <svg v-if="data.answerIncorrect" class="result_shape xmark m-auto absolute z-20"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                    <circle class="result_circle xmark_circle" cx="26" cy="26" r="25" fill="none" />
-                    <path class="result_outline" fill="none" d="M16 16 36 36 M36 16 16 36" />
-                  </svg>
-                </div>
-
-              </div>
-              <div class="card bg-[rgba(32,32,32,0.35)] rounded-box grid grow place-items-center p-6 max-w-[368px]">
-                <ArtistCard v-if="data.gameActive" :artist="data.artistsList[1]" :showListeners="showListeners" />
-              </div>
-            </div>
-            <div class="mt-5">
-              <!-- Higher/Lower guess buttons -->
-              <p v-if="!data.showGameOverMessage">{{ data.artistsList[1].info.name }}'s monthly listeners is higher or
-                lower
-                than
-                {{ data.artistsList[0].info.name }}..</p>
-              <div v-if="!data.showGameOverMessage" class="flex gap-4 justify-center mt-3">
-                <button class="btn btn-success disabled:opacity-50" @click="answerHigher"
-                  :disabled="data.answerIncorrect || data.answerCorrect">Higher</button>
-                <button class="btn btn-error disabled:opacity-50" @click="answerLower"
-                  :disabled="data.answerIncorrect || data.answerCorrect">Lower</button>
-              </div>
-            </div>
-            <!-- Start/new game buttons -->
-            <div>
-              <!-- Game over message -->
-              <h3 v-if="data.showGameOverMessage" class="text-[#c15142] font-bold">Game Over</h3>
-              <button v-if="data.showGameOverMessage" class="btn btn-success disabled:opacity-50 mt-3"
-                @click="newGame">New Game</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- Game Over -->
+    <GameOver v-if="gameManagerStore.currentScreen === 'gameOver'" />
   </div>
 </template>
 
 <style scoped>
-#artist-two {
-  margin: auto
-}
-
-@media (min-width: 640px) {
-  #artist-two {
-    margin: none;
-  }
-}
-
 .result_circle {
   stroke-dasharray: 166;
   stroke-dashoffset: 166;
--- frontend/src/components/ArtistCard.vue
+++ frontend/src/components/ArtistCard.vue
@@ -1,64 +1,35 @@
 <script setup>
-import { reactive, ref, onMounted, computed } from 'vue'
-
-const data = reactive({
-
-})
+import ArtistOverlay from './ArtistOverlay.vue'
 
 const props = defineProps({
-  artist: Object,
-  showListeners: Boolean,
+    artist: Object,
+    showListeners: Boolean,
 })
-
-const listenerText = computed(() => {
-  return props.artist?.listeners + ' monthly listeners'
-})
-
-onMounted(async () => {
-  // console.log("artist:", props.artist);
-})
-
 </script>
 
 <template>
-  <div>
-    <div v-if="props.artist">
-      <!-- Artist name -->
-      <h2 class="text-xl">{{ props.artist.info.name }}</h2>
+    <div class="relative w-full h-full overflow-hidden">
+        <div v-if="props.artist" class="w-full h-full relative">
+            <!-- Background image style -->
+            <div class="absolute inset-0 bg-center bg-cover bg-no-repeat"
+                :style="`background-image: url(${artist.info.images[1].url})`"></div>
 
-      <!-- Artist picture -->
-      <div class="my-3 transition-all">
-        <img class="m-auto w-80 h-80 rounded-xl shadow-lg" :src="artist.info.images[1].url">
-      </div>
+            <ArtistOverlay :artist="props.artist" :show-listeners="showListeners" />
+        </div>
 
-      <!-- Artist listeners -->
-      <p v-if="props.showListeners" class="flex gap-2 items-center justify-center">{{ listenerText }}
-        <a :href="artist.info.external_urls.spotify" class="tooltip" data-tip="Listen on Spotify" target="_blank"
-          rel="noopener noreferrer">
-          <svg xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="-20 50 250 100">
-            <path
-              d="M177.707 98.987c-35.992-21.375-95.36-23.34-129.719-12.912-5.519 1.674-11.353-1.44-13.024-6.958-1.672-5.521 1.439-11.352 6.96-13.029 39.443-11.972 105.008-9.66 146.443 14.936 4.964 2.947 6.59 9.356 3.649 14.31-2.944 4.963-9.359 6.6-14.31 3.653m-1.178 31.658c-2.525 4.098-7.883 5.383-11.975 2.867-30.005-18.444-75.762-23.788-111.262-13.012-4.603 1.39-9.466-1.204-10.864-5.8a8.717 8.717 0 015.805-10.856c40.553-12.307 90.968-6.347 125.432 14.833 4.092 2.52 5.38 7.88 2.864 11.968m-13.663 30.404a6.954 6.954 0 01-9.569 2.316c-26.22-16.025-59.223-19.644-98.09-10.766a6.955 6.955 0 01-8.331-5.232 6.95 6.95 0 015.233-8.334c42.533-9.722 79.017-5.538 108.448 12.446a6.96 6.96 0 012.31 9.57M111.656 0C49.992 0 0 49.99 0 111.656c0 61.672 49.992 111.66 111.657 111.66 61.668 0 111.659-49.988 111.659-111.66C223.316 49.991 173.326 0 111.657 0"
-              fill="#1ed660" />
-          </svg>
-        </a>
-      </p>
-      <p v-else class="h-6"></p>
-
+        <!-- Loading spinner -->
+        <div v-else role="status" class="w-full h-full flex justify-center items-center">
+            <svg class="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-green-400" viewBox="0 0 100 101">
+                <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor" />
+                <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill" />
+            </svg>
+        </div>
     </div>
-
-    <!-- Loading spinner for artist one -->
-    <div v-else role="status">
-      <svg class="inline mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-green-400"
-        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-          fill="currentColor" />
-        <path
-          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-          fill="currentFill" />
-      </svg>
-    </div>
-  </div>
 </template>
+
 
 <style scoped></style>
