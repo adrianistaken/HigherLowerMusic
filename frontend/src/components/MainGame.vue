022e5f02ec69322f623bff5d8f9b25bf6838ddd6
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-17 21:36:09 -0500

    Create MainGame component

---
+++ frontend/src/components/MainGame.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ArtistCard from './ArtistCard.vue';
+import { useGameManagerStore } from '@/stores/gameManagerStore'
+const gameManagerStore = useGameManagerStore();
+</script>
+
+<template>
+    <div class="w-full min-h-screen">
+        <div class="flex flex-col md:flex-row h-screen">
+            <div class="flex-1">
+                <ArtistCard :artist="gameManagerStore.firstArtist" :showListeners="true" />
+            </div>
+            <div class="flex-1">
+                <ArtistCard :artist="gameManagerStore.secondArtist" :showListeners="false" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
