23a9bb6ddb0d23da87cced7121010b8d90e7747b
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-17 21:34:43 -0500

    Install Pinia

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
@@ -12,6 +13,11 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
