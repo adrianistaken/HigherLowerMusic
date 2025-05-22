99a13bb6b9dd7640fd6e62657b9d35e2f009b088
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-21 22:45:44 -0500

    Update main menu

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -8,13 +8,17 @@ const gameManagerStore = useGameManagerStore();
         <!-- Hero section -->
         <div class="hero bg-base-200/0 min-h-screen">
             <div class="hero-content text-center">
-                <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Higher Lower Music</h1>
-                    <p class="py-6">
-                        Guess if the second artist has a higher or lower monthly listener count than the
-                        first artist! (based on Spotify's updated monthly listener count)
+                <div class="max-w-md leading-none">
+                    <h1 class="text-[50px] font-bold text-red-400">
+                        <spand class="text-green-400">HIGHER</spand> LOWER
+                    </h1>
+                    <h1 class="text-[118px] font-bold">MUSIC</h1>
+                    <h2 class="text-2xl font-bold mt-3">Who has more listeners?</h2>
+                    <p class="py-6 text-xl">
+                        Guess which music artist has more monthly Spotify listeners?
                     </p>
-                    <button class="btn btn-primary" @click="gameManagerStore.startFirstGame()">Start Guessing</button>
+                    <button class="btn btn-primary p-6 text-lg" @click="gameManagerStore.startFirstGame()">Start
+                        Guessing</button>
                 </div>
             </div>
         </div>
