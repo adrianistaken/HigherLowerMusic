d3ccb39d0b1e079e2b934d850f2c533a508ce62e
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-23 21:02:07 -0500

    Update wording on mainmenu

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -15,7 +15,7 @@ const gameManagerStore = useGameManagerStore();
                     <h1 class="text-[118px] font-bold">MUSIC</h1>
                     <h2 class="text-2xl font-bold mt-3">Who has more listeners?</h2>
                     <p class="py-6 text-xl">
-                        Guess which music artist has more monthly Spotify listeners?
+                        Guess which music artist has more monthly Spotify listeners!
                     </p>
                     <button class="btn btn-primary p-6 text-lg" @click="gameManagerStore.startFirstGame()">Start
                         Guessing</button>
